8d26df22f9d36edc912880459f406031e9d10791
xxt1205 <xxt1205@gmail.com>
2023-02-01 00:02:07 +0900

    백엔드: Study.class @Inheritance(strategy = InheritanceType.SINGLE_TABLE) 관계 제거, 객체 Wrapper 클래스로 변경, 메소드 이름 변경 saveType => createStudy.

--- src/main/java/SNP/management/domain/entity/study/DType.java
+++
@@ -1,9 +0,0 @@
-package SNP.management.domain.entity.study;
-
-import javax.persistence.*;
-
-@Entity
-@DiscriminatorValue("D_CLASS")
-public class DType extends Study{
-
-}
--- src/main/java/SNP/management/domain/entity/study/Study.java
+++ src/main/java/SNP/management/domain/entity/study/Study.java
@@ -7,34 +7,33 @@ import lombok.*;
 import javax.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "studyType", discriminatorType = DiscriminatorType.STRING)
-@Data
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "STUDY")
-public abstract class Study extends BaseEntity {
+@Getter
+public class Study extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "study_id")
     private Long id;
-    private int step;
+    private Integer step;
     private String level;
     private String detail;
     @Column(name = "number_of_days")
-    private int numberOfDays;
-    private int accumulation;
+    private Integer numberOfDays;
+    private Integer accumulation;
     @Column(name = "ot")
-    private boolean OT;
+    private Boolean OT;
     private String introduce;
 
-    private int stepCount;
+    private Integer stepCount;
 
     @Column(name = "study_type")
     @Enumerated(EnumType.STRING)
     private StudyType studyType;
 
 
-    public Study saveType(int step, String level, String detail, int numberOfDays, int accumulation, boolean OT, String introduce) {
+
+    public Study createStudy(Integer step, String level, String detail, Integer numberOfDays, Integer accumulation, Boolean OT, String introduce) {
         this.step = step;
         this.level = level;
         this.detail = detail;
